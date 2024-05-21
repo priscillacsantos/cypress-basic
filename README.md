79e96f8020ee0a9fa8c64410d76e4e94f011b588
priscillasantos <priscila.santos@joyjet.com>
2024-05-21 13:01:33 -0300

    Create cypress project

--- README.md
+++ README.md
@@ -1,29 +1,37 @@
 # Testes automatizados com Cypress - Básico
 
-👋 Seja bem-vindo(a)!
+Sample project for the basic course of the Talking About Testing online school.
 
-É muito bom tê-lo(a) aqui. Tenho certeza que você vai adorar este curso. ❤️
 
-## O que você vai aprender
+## Pre-requirements
 
-Durante o curso de testes automatizados com Cypress (básico), você vai aprender:
+It is required to have Node.js and npm installed to run this project.
 
-- Como configurar um projeto Cypress do zero
-- Como visitar páginas locais e remotas
-- Como lidar com os elementos mais comuns encontrados em aplicações web
-- Como testar _upload_ de arquivos
-- Como realizar as mais diversas verificações de resultados esperados
-- Como criar comandos customizados
-- Como lidar com links que abrem em outra aba do navegador
-- Como rodar testes simulando as dimensões de um dispositivo móvel
-- Como resolver os mesmos problemas de diferentes formas, conhecendo a [API do Cypress](https://docs.cypress.io/api/table-of-contents)
-- Como executar os testes em um _pipeline_ de integração contínua sempre que mudanças ocorrerem no código da aplicação (ou dos testes)
-- Como criar uma documentação mínima para seu projeto de testes automatizados
+I used versions 'v16.20.2' and '8.19.4' of Node.js and npm, respectively. I suggest you use the same or later versions.
 
-## Vamos começar?
+For git, I'm using version: 2.45.0.
+Cypress version: 9.5.1 [From the project root, run the command npm install cypress@9.5.1 --save-dev (or npm i cypress@9.5.1 -D for the short version)]
 
-Vá para a seção [estrutura do curso](./lessons/_course-structure_.md).
+## Installation
 
-___
+Run 'npm install' (or npm i for the short version) to install the dev dependencies.
 
-Este é mais um curso da [**Escola Talking About Testing**](https://udemy.com/user/walmyr).
+
+## Tests
+
+You can run the tests simulating a desktop ow mobile viewport.
+
+### Desktop
+
+Run 'npm run test' (or 'npm t' for the short version) to run the test in headless mode on a desktop viewport.
+
+Or, run 'npm run cy:open' to open Cypress in interactive mode on a desktop viewport.
+
+### Mobile
+
+Run 'npm run test:mobile' to run the test in headless mode on a mobile viewport.
+
+Or, run 'npm run cy.open.mobile' to open Cypress in interactive mode on a mobile viewport.
+
+
+## Support this project
---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,172 @@
+// CAC-TAT.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+
+// a linha abaixo é um comentário especial. Está dizendo p cypress buscar como referencia os tipos do cypress. (quando começo a digitar, vai ter autocomplete, consegue ver as assinatura das funções...)
+/// <reference types="Cypress" />
+
+//O bloco describe define a suíte de testes, e o bloco it, define um caso de teste.
+describe('Central de Atendimento ao Cliente TAT', function() {
+
+    beforeEach (function() {
+        cy.visit ('./src/index.html');
+    })
+
+    it('verifica o título da aplicação', function() {
+        cy.title().should ('be.equal', 'Central de Atendimento ao Cliente TAT');
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'hi pecial character sequw, home, end, insert, pageUp, pageDownalt, option, ctrl, control, meta, command, cmd, shift If you want to skip parsing special character sequences and type the text exactly as written';
+        cy.get('input[id="firstName"]').type('Ana');
+        cy.get('input[id="lastName"]').type('Mala');
+        cy.get('input[id="email"]').type('e@e.com');
+        cy.get('textarea[id="open-text-area"]').type(longText, {delay: 0});
+        cy.get('button[type="submit"]').click();
+        cy.get('span[class="success"]').should('be.visible'); 
+        //outra forma de chamar uma classe é assim: cy.get('.sucess').should('be.visible'); 
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.get('input[id="firstName"]').type('Ana');
+        cy.get('input[id="lastName"]').type('Mala');
+        cy.get('input[id="email"]').type('ee.com');
+        cy.get('textarea[id="open-text-area"]').type('test');
+        cy.get('button[type="submit"]').click();
+        cy.get('.error').should('be.visible');
+    })
+
+    it('validar que, se um valor não-numérico for digitado, seu valor continuará vazio', function() {
+        cy.get('#phone')
+        .type('Ana')
+        .should('have.value', '');
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('input[id="firstName"]').type('Ana');
+        cy.get('input[id="lastName"]').type('Mala');
+        cy.get('input[id="email"]').type('e@e.com');
+        cy.get('#phone-checkbox').click();
+        cy.get('textarea[id="open-text-area"]').type('test');
+        cy.get('button[type="submit"]').click();
+        cy.get('.error').should('be.visible');
+        //cy.get('.phone-label-span required-mark').should
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('input[id="firstName"]')
+          .type('Ana')
+          .should('have.value', 'Ana')
+          .clear()
+          .should('have.value', '');
+        cy.get('input[id="lastName"]')
+          .type('Mala')
+          .should('have.value', 'Mala')
+          .clear()
+          .should('have.value', '');
+        cy.get('input[id="email"]')
+          .type('e@e.com')
+          .should('have.value', 'e@e.com')
+          .clear()
+          .should('have.value', '');
+        cy.get('#phone')
+        .type('1234567')
+          .should('have.value', '1234567')
+          .clear()
+          .should('have.value', '');
+        // cy.get('textarea[id="open-text-area"]').type('test');
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.get('button[type="submit"]').click();
+        cy.get('.error').should('be.visible');
+    })
+
+    it('envia o formulário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSubmit();
+        cy.get('button[type="submit"]').click();
+        cy.get('.success').should('be.visible');
+    })
+
+    it('primeiro uso do cy-contains', function() {
+        cy.fillMandatoryFieldsAndSubmit();
+        cy.contains('button', 'Enviar').click();
+        cy.get('.success').should('be.visible');
+    })
+
+    it('teste do comado select: seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('select').select('YouTube').should('have.value', 'youtube');
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('select').select('mentoria').should('have.value', 'mentoria');
+    })
+
+    it('seleciona um produto (Blog) por seu índice', function() {
+        cy.get('select').select(1).should('have.value', 'blog');
+    })
+
+    it('marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"]').check('feedback').should('have.value', 'feedback');
+        //outra forma de fazer: cy get('input[type="radio"][value="feedback"]').check....
+    })
+
+    it('marca cada tipo de atendimento', function() {
+        cy.get('input[type="radio"]')
+          .should('have.length', 3)
+          .each(function($radio) {
+            cy.wrap($radio).check();
+            cy.wrap($radio).should('be.checked');
+          })
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function() {
+        cy.get('[type="checkbox"]').check().should('be.checked');
+        cy.get('#phone-checkbox').uncheck().should('not.be.checked');
+        //ou: cy.get('[type="checkbox"]').check().should('be.checked').last().uncheck().should('not.be.checked');
+    })
+
+    it('seleciona um arquivo da pasta fixtures', function() {
+        cy.get('#file-upload')
+          .should('not.have.value')
+          .selectFile('cypress/fixtures/example.json')
+          .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+          })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function() {
+        cy.get('#file-upload')
+          .should('not.have.value')
+          .selectFile('cypress/fixtures/example.json', {action: 'drag-drop'})
+          .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+          })
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+        cy.fixture("example.json").as('sampleFile');
+        cy.get('input[type="file"]')
+          .selectFile('@sampleFile')
+          .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+          })
+    })
+
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+      cy.get('#privacy a').should('have.attr', 'target', '_blank')
+    })
+    
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
+      cy.get('#privacy a')
+        .invoke('removeAttr', 'target')
+        .click()
+
+      cy.contains('Talking About Testing').should('be.visible')
+    })
+
+  })
